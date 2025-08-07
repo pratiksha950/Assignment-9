574303a894f9a5d1447b15ecb16769c42f7fb857
pratiksha950 <salunkepratiksha950@gmail.com>
2025-08-07 18:35:11 +0530

    solution with proper function added

--- Assignment-9.js
+++ Assignment-9.js
@@ -1,14 +1,23 @@
 function area(){
     const input1=document.getElementById("input1");
     const input2=document.getElementById("input2");
-    const result=document.getElementById("button");
-    const perimeter=2(parseInt(input1.value)+parseInt(input2.value));
+    const solution=document.getElementById("solution");
+    let length=parseInt(input1.value);
+    let width=parseInt(input2.value);
+    const perimeter=2 *(length+width);
+
+    if(isNaN(length)||isNaN(width)){
+        solution.innerText = "Please enter valid numbers.";
+        return;
+    }
+
+    solution.innerText=`perimeter is ${perimeter}`;
 
 }
 function clearInput(){
     const input1=document.getElementById("input1");
     const input2=document.getElementById("input2");
-    const solution=document.getElementById("button");
+    const solution=document.getElementById("solution");
 
     input1.value="";
     input2.value="";
